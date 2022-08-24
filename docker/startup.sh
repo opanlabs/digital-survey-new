68a857a8324e7a74edfa9d6b33536bbefbf2b7e9
Riski Dwi Sabariyanto <bintangmalamhari@gmail.com>
2022-08-25 01:58:14 +0700

    docker rev 19

--- docker/startup.sh
+++ docker/startup.sh
@@ -8,9 +8,9 @@ php-fpm -D
 
 ls
 
-chmod -R 777 storage/app/public
+chmod -R 777 storage/app/public/images
 
-chmod -R 777 public/storage
+chmod -R 777 public/storage/images
 
 echo -e "success chmod bos"
 
