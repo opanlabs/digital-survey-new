c7abeae20a7ea7ac79246200d712e16033a93270
Riski Dwi Sabariyanto <bintangmalamhari@gmail.com>
2022-08-25 06:36:44 +0700

    docker rev 33

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,20 +6,4 @@ php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
-ls
-
-php artisan storage:link
-
-mkdir -p storage/app/public/images
-
-# setfacl -PRdm u::rwx,g::rwx,o::rw storage/app/public/images
-
-# setfacl -PRdm u::rwx,g::rwx,o::rw public/storage/images
-
-chmod -R 777 storage/app/public/images
-
-chmod -R 777 public/storage/images
-
-echo -e "success chmod bos"
-
 nginx
