bfe705dea945b1cfa1ec6e7c6e27e7f2b0f387af
Riski Dwi Sabariyanto <bintangmalamhari@gmail.com>
2022-08-25 01:15:35 +0700

    docker rev.15

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,6 +6,14 @@ php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
+ls
+
+sudo -u www-data stat /username/test/static
+
+gpasswd -a www-data username
+
+chmod g+x /username && chmod g+x /username/test && chmod g+x /username/test/static
+
 chmod -R 777 /app/storage/app/public
 
 chmod -R 777 /app/public/storage
@@ -13,5 +21,3 @@ chmod -R 777 /app/public/storage
 echo -e "success chmod"
 
 nginx
-
-
